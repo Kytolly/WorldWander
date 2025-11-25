e90dfc32e64443b4580fab667802bacaeaf3745a
lulupig12138 <QuintonSung@outlook.com>
2025-11-25 10:50:23 +0000

    1125

--- scripts/inference_wan2.sh
+++ scripts/inference_wan2.sh
@@ -2,11 +2,11 @@ export PYTHONPATH=$(pwd)
 
 
 # three2one, synthetic scenarios
-CUDA_VISIBLE_DEVICES=1 python src/wan2_inference.py --config=configs/wan2-2_lora_three2one_synthetic.yaml \
-                        --ckpt_path="ckpts/wan2-2_lora_three2one_synthetic.ckpt" \
-                        --original_video_root="examples/synthetic_scenarios/third_video" \
-                        --pred_path="pred_results/three2one_synthetic" \
-                        --seed=1234
+# CUDA_VISIBLE_DEVICES=1 python src/wan2_inference.py --config=configs/wan2-2_lora_three2one_synthetic.yaml \
+#                         --ckpt_path="ckpts/wan2-2_lora_three2one_synthetic.ckpt" \
+#                         --original_video_root="examples/synthetic_scenarios/third_video" \
+#                         --pred_path="pred_results/three2one_synthetic" \
+#                         --seed=1234
 
 
 # one2three, synthetic scenarios
@@ -17,11 +17,19 @@ CUDA_VISIBLE_DEVICES=1 python src/wan2_inference.py --config=configs/wan2-2_lora
 #                         --pred_path "pred_results/one2three" \
 #                         --seed=1234
 
+
 # three2one, real-world scenarios
 # CUDA_VISIBLE_DEVICES=6 python src/wan2_inference.py --config=configs/wan2-2_lora_three2one_realworld.yaml \
-#                         --ckpt_path="ckpts/wan2-2_lora_three2one_synthetic.ckpt" \
-#                         --original_video_root="/mnt/nfs/workspace/sqj/EgoExoTestCrop/Third_Video" \
-#                         --pred_path="pred_results/tmp" \
+#                         --ckpt_path="ckpts/wan2-2_lora_three2one_realworld.ckpt" \
+#                         --original_video_root="examples/realworld_scenarios/third_video" \
+#                         --pred_path="pred_results/tmp/three2one_realworld" \
 #                         --seed=1234
 
-                        
+
+# one2three, real-world scenarios
+CUDA_VISIBLE_DEVICES=3 python src/wan2_inference.py --config=configs/wan2-2_lora_one2three_realworld.yaml \
+                        --ckpt_path "ckpts/wan2-2_lora_three2one_realworld.ckpt" \
+                        --original_video_root="examples/realworld_scenarios/First_Video" \
+                        --ref_image_root="examples/realworld_scenarios/Reference_Image" \
+                        --pred_path "pred_results/one2three_realworld" \
+                        --seed=1234
